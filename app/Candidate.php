9e7297b42cc4fe59d0a9ea1e886b8ae9e714aa92
Ruslan Ponomarev <ConductorDeSistemasWeb@gmail.com>
2019-05-16 10:23:37 +0300

    Define data in models

--- app/Candidate.php
+++ app/Candidate.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Candidate extends Model
 {
-    //
+    protected $fillable = ['first', 'last', 'email'];
+
+    /**
+     * Get all of the posts for the user.
+     */
+    public function jobs()
+    {
+        return $this->hasMany('App\Job');
+    }
 }
+
--- app/Job.php
+++ app/Job.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    //
+    protected $fillable = ['candidate_id', 'position', 'company', 'start_at', 'end_at'];
 }
